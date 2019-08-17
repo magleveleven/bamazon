5a2fa6236f1b05a731487363b5426d95e823c532
Eric Ballesteros <ballesteros@icloud.com>
2019-08-17 17:19:45 -0500

    added data, inquirer prompts, and functions for list and buy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,18 +22,86 @@ var start = function(){
         choices: ["VIEW", "BUY"]
     }).then(function(answer){
         if(answer.viewOrBuy.toUpperCase()=="VIEW"){
-            //showInventory();
+            showInventory();
         }
         else {
-            //buyProduct();
+            buyProduct();
         }
     })
 }
 
 var showInventory = function() {
-
+    connection.query("SELECT * FROM products",  function(err,rows){
+        if(err) {
+            console.log(err);
+            return;}
+        //console.log(res);
+        rows.forEach(function(res) {
+            console.log(res.id, res.product_name, res.department_name, res.price, res.quantity)})
+            inquirer.prompt({
+                name: "enterIDtoBuy",
+                type: "input",
+                message: "Enter the ID of the product you would like to purchase.",
+                validate: function(value){
+                    if(isNaN(value)==false){
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }}
+            }).then(function(answer){
+                console.log(answer);
+                inquirer.prompt({
+                    name: "enterQuantity",
+                    type: "input",
+                    message: "And how many you would like to purchase?",
+                    validate: function(value){
+                        if(isNaN(value)==false){
+                            return true;
+                        }
+                        else {
+                            return false;
+                        }
+                    }
+                })
+            })
+        })
 }
 
 var buyProduct = function() {
-    
+    connection.query("SELECT * FROM products",  function(err,rows){
+        if(err) {
+            console.log(err);
+            return;}
+        //rows.forEach(function(res) {
+            //console.log(res.id, res.product_name, res.department_name, res.price, res.quantity);
+            inquirer.prompt({
+                name: "enterIDtoBuy",
+                type: "input",
+                message: "Enter the ID of the product you would like to purchase.",
+                validate: function(value){
+                    if(isNaN(value)==false){
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            })
+            .then(function(answer){
+            console.log(answer);
+            inquirer.prompt({
+                name: "enterQuantity",
+                type: "input",
+                message: "And how many you would like to purchase?",
+                validate: function(value){
+                    if(isNaN(value)==false){
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }}
+                })
+            })
+        })
 }
